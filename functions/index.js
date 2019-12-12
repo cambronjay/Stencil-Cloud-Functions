7505330fb5567b05a7895a47582a81788df11398
cambronjay <cambronjay@gmail.com>
2019-12-12 16:15:36 -0500

    finished

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
 'use strict';
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const Unsplash = require('unsplash-js').default;
-const unsplash = new Unsplash({ accessKey: "3831e59ad01ee4cdce431285c5c45fbed3c531cd7aaa67219f56383462ca777e", timeout: 1000 });
 const cors = require('cors')({
     origin: true,
 });
 admin.initializeApp(functions.config().firebase);
-// const axios = require('axios').default;
-// const instance = axios.create();
+const axios = require('axios').default;
+const token = "3831e59ad01ee4cdce431285c5c45fbed3c531cd7aaa67219f56383462ca777e";
+const instance = axios.create();
 
 exports.LoadPictures = functions.https.onRequest((request, response) => {
     if (request.method !== 'GET') {
@@ -17,14 +16,13 @@ exports.LoadPictures = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
         const page = request.query.page;
         try {
-            const g = unsplash.search.photos("mountains", page, 15, { orientation: "portrait" })
-                .then(toJson)
-                .then(json => {
-                    return response.status(200).send(json);
+            const g = instance.get(`https://api.unsplash.com/search/photos/?client_id=3831e59ad01ee4cdce431285c5c45fbed3c531cd7aaa67219f56383462ca777e&page=${page}&query=mountains`)
+                .then((data) => {
+                    return response.status(200).send(data.data);
                 })
                 .catch((error) => {
                     response.sendStatus(500);
-                });
+                })
         } catch (error) {
             response.sendStatus(500);
         }
